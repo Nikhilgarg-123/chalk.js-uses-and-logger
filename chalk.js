2ba258362b8655360a6549ac5eb26cc258dd9cb8
Nikhil Garg <nikhilgarg.drb@gmail.com>
2024-08-27 23:42:00 +0530

    add basic files

---
+++ chalk.js
@@ -0,0 +1,9 @@
+const chalk = require('chalk');
+console.log(chalk.blue('Hello world!'));
+const log = console.log;
+
+// Combine styled and normal strings
+log(chalk.blue('Hello') + ' World' + chalk.red('!'));
+
+// Compose multiple styles using the chainable API
+log(chalk.blue.bgRed.bold('Hello world!'));
